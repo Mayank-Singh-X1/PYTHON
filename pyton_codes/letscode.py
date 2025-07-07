e671adb1365cfd0ff53b0b2d1804c72eb1335936
Mayank-Singh <mayankbrawler@gmail.com>
2025-07-07 18:50:33 +0530

    missing so amny days

--- pyton_codes/letscode.py
+++ pyton_codes/letscode.py
@@ -1,3 +1,9 @@
-fruit=["apple","banana","chickoo"]
-for i in fruit:
-  print(i)
+# fruit=["apple","banana","chickoo"]
+# for i in fruit:
+#   print(i)
+def fnc(n1=23,name='chuitya'):
+  print(f"my name is {name} and {n1}")
+
+fnc("choot")
+square=lambda x:2*2
+print(square(2))
