ba28603cc3562168abb186a3666fe27802a107e2
Mayank-Singh <mayankbrawler@gmail.com>
2025-07-08 17:41:20 +0530

    starting todays session

--- pyton_codes/letscode.py
+++ pyton_codes/letscode.py
@@ -1,14 +1,18 @@
-word=" pyhton "
-a=word.strip()
+# word=" pyhton "
+# a=word.strip()
+# print(a)
+
+# url="https://war.compile"
+# b=url.removeprefix("https://")
+# print(b)
+# print(4-5)
+
+# c=10_234_344
+# print(c)
+# CAT_SOUND="meow"
+# CAT_SOUND="bahw"
+# print(CAT_SOUND)
+
+#lists
+a=["bicycles","choot","boom"]
 print(a)
-
-url="https://war.compile"
-b=url.removeprefix("https://")
-print(b)
-print(4-5)
-
-c=10_234_344
-print(c)
-CAT_SOUND="meow"
-CAT_SOUND="bahw"
-print(CAT_SOUND)
