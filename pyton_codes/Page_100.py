703ff9b14e20b62fec7b00deb9b15c610487d0ab
Mayank-Singh <mayankbrawler@gmail.com>
2025-07-14 18:05:54 +0530

    start here 130

--- pyton_codes/Page_100.py
+++ pyton_codes/Page_100.py
@@ -1,28 +1,30 @@
-# car="audi"
-# print(car.title()=="Audi
-# div=[x for x in range (0,100) if x%5==0]
-# print(div)
+# # car="audi"
+# # print(car.title()=="Audi
+# # div=[x for x in range (0,100) if x%5==0]
+# # print(div)
 
-# print((5>3) and (4<5))
+# # print((5>3) and (4<5))
 
-# toppings=["sauce","onions","milk"]
-# print("milkw" not in toppings)
+# # toppings=["sauce","onions","milk"]
+# # print("milkw" not in toppings)
 
-a=50
-if (a<18):
-  print("hello")
-elif(a<10):
-  print("boom")
-else:('kaboom')
+# a=50
+# if (a<18):
+#   print("hello")
+# elif(a<10):
+#   print("boom")
+# else:('kaboom')
 
-available_toppings = ['mushrooms', 'olives', 'green peppers','pepperoni', 'pineapple', 'extra cheese']
+# available_toppings = ['mushrooms', 'olives', 'green peppers','pepperoni', 'pineapple', 'extra cheese']
 
-requested_toppings = ['mushrooms', 'olives', 'green peppeers']
+# requested_toppings = ['mushrooms', 'olives', 'green peppeers']
 
-for requested_topping in requested_toppings:
-  if requested_topping in available_toppings:
-       print(f"adding {requested_topping} to your pizza")
-  else:
-      print(f"sorry we ran out of {requested_topping}")
+# for requested_topping in requested_toppings:
+#   if requested_topping in available_toppings:
+#     print(f"adding {requested_topping} to your pizza")
+#   else:
+#     print(f"sorry we ran out of {requested_topping}")
 
-print("hello")
+# print("hello")
+
+
