9adf7bc9e2cbd43295356d2345d0a9d80e7bd819
Mayank-Singh <mayankbrawler@gmail.com>
2025-07-15 19:19:54 +0530

    break

--- pyton_codes/Page_100.py
+++ pyton_codes/Page_100.py
@@ -28,7 +28,7 @@
 # print("hello")
 
 #DICTIONARIES
-alien={"color":"green","points":5,"speed":"medium","x_pos":0,"y_pos":10}
+
 # if alien["speed"]=="medium":
 #     alien["x_pos"] +=2
 
@@ -36,6 +36,40 @@ alien={"color":"green","points":5,"speed":"medium","x_pos":0,"y_pos":10}
 # print(alien)
 
 
-for key in alien:
-    print(f"{key} \n")
+# for key in alien:
+#     print(f"{key} \n")
 
+# a=[1,2,3,44,55,6,6,6,7,7,8]
+# print(set(a))
+# river={"nile":"egypt","ganga":"india","yamuna":'bihar'}
+# for x,y in river.items():
+#     print(f"The {x} runs through {y}")
+
+# alien=[]
+# for i in range(30):
+#     new_alien={"color":"green","points":5,"speed":"medium"}
+#     alien.append(new_alien)
+
+# for k in alien[:5:2]:
+#     print(k)
+
+# aliens = []
+#  # Make 30 green aliens.
+# for alien_number in range(30):
+#     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+#     aliens.append(new_alien)
+#  # Show the first 5 aliens.
+# for alien in aliens[:5]:
+#     print(alien)
+# print("...")
+
+# for i in range(0,100,2):
+#     print(i)
+
+pizza={
+    "crust":"thick",
+    "toppings":["cheese","pepperoni"]
+}
+for i in pizza["toppings"]:
+    print(i)
+print(f"you ordered crust {pizza["crust"]} with toppings {i}")
--- pyton_codes/tempCodeRunnerFile.py
+++ pyton_codes/tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-print(alien)
+alien=[]
+for i in range(30):
+    new_alien={"color":"green","points":5,"speed":"medium"}
+    alien.append(new_alien)
+
+for k in alien[:5]:
+    print(k)
