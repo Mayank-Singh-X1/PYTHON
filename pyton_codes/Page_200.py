f29949639b05ff3cb71b107f6368acd51b8f0dce
Mayank-Singh <mayankbrawler@gmail.com>
2025-07-24 17:49:21 +0530

    break

--- pyton_codes/Page_200.py
+++ pyton_codes/Page_200.py
@@ -1,10 +1,34 @@
-class dog:
-    def __init__(self,name,age):
-        self.Name=name
-        self.Age=age
+# class dog:
+#     def __init__(self,name,age):
+#         self.Name=name
+#         self.Age=age
 
-    def sit(self):
-        print(f"{dog.Name} is sitting")
+#     def sit(self):
+#         print(f"{self.Name} is sitting")
 
-    def rollover(self):
-        print(f"{dog.Age} is jutst a nmber")
+#     def rollover(self):
+#         print(f"{self.Age} is jutst a nmber")
+
+# mydog=dog('madharchod', 45)
+# mydog=dog('piyush',3)
+# print(f"{mydog.Name} is name and age is {mydog.Age}")
+# mydog.sit()
+# mydog.rollover()
+# print(mydog.Name)
+
+class restaurants:
+    def __init__(self,restaurant_name, cuisine ):
+        self.name=restaurant_name
+        self.Cuisine=cuisine
+
+    def descibe_restaurant(self):
+        print("Best restaurant in world")
+
+    def open_restaurant(self):
+        print("Resaturant is open")
+
+a=restaurants("reyansh","Italinan")
+print(f"name is {a.name} and cuisine is {a.Cuisine}")
+
+a.descibe_restaurant()
+a.open_restaurant()
