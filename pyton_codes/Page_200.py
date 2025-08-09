0a1d6a15524311957d3dd7b472a1fc59f0796f4d
Mayank-Singh <mayankbrawler@gmail.com>
2025-08-09 15:40:59 +0530

    Day 2

--- pyton_codes/Page_200.py
+++ pyton_codes/Page_200.py
@@ -63,31 +63,36 @@
 # print(a3)
 
 
-class user:
-    def __init__(self,firstname,lastname):
-        self.fname=firstname
-        self.lname=lastname
-        self.attemp_login=0
-        self.max=3
+# class user:
+#     def __init__(self,firstname,lastname):
+#         self.fname=firstname
+#         self.lname=lastname
+#         self.attemp_login=0
+#         self.max=3
 
-    def greet(self):
-        print(f"Hello {self.fname} {self.lname}")
+#     def greet(self):
+#         print(f"Hello {self.fname} {self.lname}")
 
-    def increment_attempt(self):
-        if self.attemp_login<3:
-            self.attemp_login+=1
-            print(f"attempts left {self.max-self.attemp_login}")
-        else:
-            print("you ran out of attempts")
+#     def increment_attempt(self):
+#         if self.attemp_login<3:
+#             self.attemp_login+=1
+#             print(f"attempts left {self.max-self.attemp_login}")
+#         else:
+#             print("you ran out of attempts")
 
-    def dis_attempt(self):
-        print(self.attemp_login)
+#     def dis_attempt(self):
+#         print(self.attemp_login)
 
-a=user('madhar','chod')
-a.greet()
-a.increment_attempt()
-a.increment_attempt()
-a.increment_attempt()
-a.increment_attempt()
+# a=user('madhar','chod')
+# a.greet()
+# a.increment_attempt()
+# a.increment_attempt()
+# a.increment_attempt()
+# a.increment_attempt()
+# a.dis_attempt()
 
-a.dis_attempt()
+# from random import choice as raand
+# l1=[1,"boom",34,"dnjcd","3445"]
+# a=raand(l1)
+# print(a)
+
--- pyton_codes/tempCodeRunnerFile.py
+++ pyton_codes/tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-self.attemp_login
+from random import choice as raand
+# l1=[1,"boom",34,"dnjcd","3445"]
+# a=raand(l1)
+# print(a)
