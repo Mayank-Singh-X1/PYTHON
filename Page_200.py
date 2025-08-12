c3d86b0c52f037248ab7b299b02325380f441a07
Mayank-Singh <mayankbrawler@gmail.com>
2025-08-12 16:01:02 +0530

    Day 3

--- Page_200.py
+++ Page_200.py
@@ -193,15 +193,17 @@
 # wer=json.loads(name)
 # print(f"we rememberred you {wer}")
 
-from pathlib import Path
-import json
-path = Path('username.json')
-if path.exists():
-    contents = path.read_text()
-    username = json.loads(contents)
-    print(f"Welcome back, {username}!")
-else:
-    username = input("What is your name? ")
-    contents = json.dumps(username)
-    path.write_text(contents)
-    print(f"We'll remember you when you come back, {username}!")
+# from pathlib import Path
+# import json
+# path = Path('username.json')
+# if path.exists():
+#     contents = path.read_text()
+#     username = json.loads(contents)
+#     print(f"Welcome back, {username}!")
+# else:
+#     username = input("What is your name? ")
+#     contents = json.dumps(username)
+#     path.write_text(contents)
+#     print(f"We'll remember you when you come back, {username}!")
+
+
